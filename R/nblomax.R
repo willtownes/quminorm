32e5936345cee58a2733be88ff35a120f4486cc9
Will Townes <will.townes@gmail.com>
2019-11-05 22:35:23 -0500

    roxygen documentation for nblomax.R

--- R/nblomax.R
+++ R/nblomax.R
@@ -9,26 +9,40 @@
 # mu = lambda*(exp(z/alpha)-1)
 # Lomax prior is conjugate to neg binom only if lambda=phi, see bnbinom.R
 
-#library(modules)
-#qmn<-import("./algs/quminorm")
-#source("./algs/quminorm.R")
-#library(Matrix)
-#library(slam)
-
-dnblomax<-function(x,tail=1,scale=1,shape=1,log=FALSE,quadpts=1000){
-  #Compute PMF of compound negative binomial distribution with Lomax mixing
-  #shape is the negative binomial dispersion parameter
-  #shape=Inf => poisson-lomax
-  #shape=1 => geometric-lomax
-  #tail>0 controls heaviness of power law tail (larger tail=lighter tail)
-  #if tail<=1 there is no mean
-  #if tail<2 there is no variance
-  #scale is a scale parameter (larger scale=fewer zeros and more large vals)
-  #if quadpts is a number, the quadrature points are recomputed based on current parameter values
-  #if quadpts is a list, we assume it is the result of a call to
-  #quadpts<-statmod::gauss.quad.prob(m, dist="uniform", l=0, u=1)
-  #where m is the number of quadrature points
-  #ie that the quadpts are pre-computed based on the Uniform(0,1) distribution
+#' @title Negative binomial-Lomax PMF
+#' @description Compute probability mass function of a negative binomial
+#'   distribution with mean parameter distributed as Lomax.
+#' @name dnblomax
+#'
+#' @param x a vector of (non-negative integer) quantiles.
+#' @param tail a positive scalar controlling the heaviness of the Lomax tail.
+#' @param scale a positive scalar.
+#' @param shape a positive scalar negative binomial parameter; can be Inf.
+#' @param log logical; if TRUE, probabilities p are given as log(p).
+#' @param quadpts either a single integer determining the number of quadrature
+#'   points, or a list resulting from
+#'   statmod::gauss.quad.prob(m, dist="uniform", l=0, u=1)
+#'   where m is the number of quadrature points.
+#'
+#' @details This function computes the likelihood the data x was generated
+#'   from the model mu~Lomax(tail,scale), x~negative binomial(shape,mu).
+#'   If the shape parameter is Inf this is a Poisson-Lomax model. If shape is 1,
+#'   it is a geometric-Lomax model. The Lomax mixing distribution induces
+#'   a heavy power-law tail with exponent (1+tail). The smaller the tail
+#'   parameter, the heavier the tail of the distribution. If tail<=1 there is no
+#'   mean, if tail<2 there is no variance. A larger scale parameter means fewer
+#'   zeros and more large values. Shape and scale have no effect on the
+#'   power-law tail. If quadpts is a number, the quadrature points are
+#'   recomputed based on current parameter values. If quadpts is a list, we
+#'   assume it is the result of a call to
+#'   quadpts<-statmod::gauss.quad.prob(m, dist="uniform", l=0, u=1)
+#'   where m is the number of quadrature points, ie that the quadpts are
+#'   pre-computed based on the Uniform(0,1) distribution.
+#'
+#' @return a vector of (log) probabilities
+#' @import statmod,matrixStats
+#' @export
+dnblomax<-function(x, tail=1, scale=1, shape=1, log=FALSE, quadpts=1000){
   if(is.list(quadpts)){
     q<-quadpts
     mu<-scale*((1-q$nodes)^(-1/tail)-1) #inverse CDF of Lomax
@@ -43,17 +57,37 @@ dnblomax<-function(x,tail=1,scale=1,shape=1,log=FALSE,quadpts=1000){
     f<-function(xi){dnbinom(xi,size=shape,mu=mu,log=TRUE)}
   }
   #approximate the log of the integral
-  g<-function(xi){matrixStats::logSumExp(w+f(xi))} 
+  g<-function(xi){matrixStats::logSumExp(w+f(xi))}
   #log PMF for all data points x and all quadrature points t
   #matrix with nrow=quadpts, ncol=length(x)
   lpmf<-vapply(x,g,FUN.VALUE=0.0)
   if(log==TRUE){ return(lpmf) } else { return(exp(lpmf)) }
 }
+
 dglomax<-function(x,...){ dnblomax(x,shape=1,...) }
+
 dplomax<-function(x,...){ dnblomax(x,shape=Inf,...) }
 
-llcurve_lomax<-function(xmax,lpar=c(1,1,1),lik=c("nb","geom","poi"),q=1000,add=TRUE,...){
-  #Draw the PMF curve on log-log axes for a Discrete-Lomax distribution 
+#' @title Log-log curve for negative binomial-Lomax PMF
+#' @description Draw the probability mass function of a negative binomial-Lomax
+#'   distribution with both horizontal and vertical axes log transformed.
+#' @name llcurve_lomax
+#'
+#' @param xmax the maximum quantile at which to evaluate the PMF.
+#' @param lpar numeric vector containing the tail, scale, and shape parameters
+#'   see \link{dnblomax} for details.
+#' @param lik choice of negative binomial, geometric, or Poisson likelihood
+#' @param q positive integer number of quadrature points, increase for better
+#'   accuracy
+#' @param add logical, should the curve be added to the existing plot window?
+#' @param ... additional arguments passed to \link[graphics]{curve}
+#'
+#' @return a list with components x and y of the points that were drawn is
+#'   returned invisibly.
+#' @export
+llcurve_lomax<-function(xmax, lpar=c(1,1,1), lik=c("nb","geom","poi"), q=1000,
+                        add=TRUE, ...){
+  #Draw the PMF curve on log-log axes for a Discrete-Lomax distribution
   #Curve goes from zero to xmax
   #lpar are the tail,scale,and shape parameters
   #q is number of quadrature points, increase for better accuracy
@@ -66,6 +100,7 @@ llcurve_lomax<-function(xmax,lpar=c(1,1,1),lik=c("nb","geom","poi"),q=1000,add=T
   curve(f,from=0,to=log1p(xmax),add=add,...)
 }
 
+#' @import statmod
 nblomax_score<-function(x,tail=1,scale=1,shape=1,quadpts=1000,fix_shape=FALSE){
   #the gradient of the log likelihood (data=x) at each data point
   #returns matrix with length(x) rows and num_params cols
@@ -90,7 +125,7 @@ nblomax_score<-function(x,tail=1,scale=1,shape=1,quadpts=1000,fix_shape=FALSE){
   G[,1]<- -log1p(mu/scale)*(mu+scale)/tail #dmu/dtail
   G[,2]<- mu/scale #dmu/dscale
   if(!fix_shape){ #part of gradient with respect to shape param
-    G[,3]<- -digamma(r)+1-log1p(r*mu) 
+    G[,3]<- -digamma(r)+1-log1p(r*mu)
   }
   if(is.infinite(r)){ #poisson
     pxmu<-function(xi){dpois(xi,mu,log=TRUE)}
@@ -112,14 +147,33 @@ nblomax_score<-function(x,tail=1,scale=1,shape=1,quadpts=1000,fix_shape=FALSE){
   res
 }
 
-nblomax_mle<-function(x,quadpts=1000,m=rep(0,3),s=rep(Inf,3),shape=NULL,om="BFGS",...){
-  #compute the MLE of parameters of negative-binomial-lomax
-  #from random sample of data x (should be integers)
-  #params are tail,scale,shape
-  #if set shape to a non null value it fixes the distribution with that shape parameter
-  #m,s are means and standard deviations of log-scale priors of parameters:
-  #m,s induce an L2 penalty
-  #... extra arguments passed to optim
+#' @title Negative binomial-Lomax MLEs
+#' @description Compute the maximum likelihood estimates for parameters of the
+#'   negative binomial-Lomax distribution.
+#' @name nblomax_mle
+#'
+#' @param x vector of non-negative integers (the data).
+#' @param quadpts positive integer, number of quadrature points.
+#' @param m numeric vector of length three. The prior means for the log of the
+#'   tail, scale, and shape parameters. See \link{dnblomax} for details.
+#' @param s numeric vector of length three. The prior standard deviations for
+#'   the log of the tail, scale, and shape parameters. If s is finite, an L2
+#'   (ridge) penalty shrinks the MLEs toward m. If s is Inf, the unpenalized MLE
+#'   is obtained.
+#' @param shape positive scalar. If NULL the shape parameter is estimated from
+#'   data. If not NULL, the shape parameter is fixed at the specified value
+#'   and only tail and scale are estimated. Shape=Inf corresponds to
+#'   Poisson-Lomax.
+#' @param om optimization method to be used by \link[stats]{optim}.
+#' @param ... additional arguments passed to \link[stats]{optim}
+#'
+#' @return a named numeric vector containing the MLEs of tail, scale, and shape
+#'   the maximized value of the log-likelihood is attached as an attribute.
+#'
+#' @import statmod
+#' @export
+nblomax_mle<-function(x, quadpts=1000, m=rep(0,3), s=rep(Inf,3), shape=NULL,
+                      om="BFGS", ...){
   xt<- table(x)/length(x) #fraction of counts for each unique value of x
   xu<- as.integer(names(xt)) #unique values of x
   xt<- as.numeric(xt)
@@ -164,9 +218,29 @@ nblomax_mle<-function(x,quadpts=1000,m=rep(0,3),s=rep(Inf,3),shape=NULL,om="BFGS
   attr(mle,"loglik")<-length(x)*res$value
   mle
 }
+
 plomax_mle<-function(x,...){ nblomax_mle(x,shape=Inf,...) }
+
 glomax_mle<-function(x,...){ nblomax_mle(x,shape=1,...) }
 
+#' @title Negative binomial-Lomax MLEs for columns of a matrix
+#' @description Compute the maximum likelihood estimates for parameters of the
+#'   negative binomial-Lomax distribution for each column of a matrix
+#' @name nblomax_mle_matrix
+#'
+#' @param m a matrix or sparse Matrix of non-negative integers (the data).
+#' @param quadpts positive integer, number of quadrature points.
+#' @param maxtry positive integer, number of times to retry fitting if initial
+#'   fit fails due to numerical problems. Each retry increases quadpts by 500.
+#' @param verbose logical, should progress messages be printed?
+#' @param ... additional arguments passed to \link{nblomax_mle}
+#'
+#' @return a data frame whose rows correspond to the columns of m.
+#'   The columns contain the MLEs for the parameters, the log-likelihood, and
+#'   the \link[stats]{BIC} values.
+#'
+#' @import slam
+#' @export
 nblomax_mle_matrix<-function(m,quadpts=1000,maxtry=10,verbose=FALSE,...){
   #m a matrix of counts (0,1,2,...)
   #Computes the maximum likelihood estimates for neg binom-lomax fit
@@ -206,11 +280,34 @@ nblomax_mle_matrix<-function(m,quadpts=1000,maxtry=10,verbose=FALSE,...){
   res$bic<- -2*res$loglik + log(nrow(m))*(ncol(res)-1)
   res
 }
+
 plomax_mle_matrix<-function(m,...){ nblomax_mle_matrix(m,shape=Inf,...) }
+
 glomax_mle_matrix<-function(m,...){ nblomax_mle_matrix(m,shape=1,...) }
 
-pglomax_pzero2scale<-function(lpz,lik=c("poisson","geometric"),tail=0.8,quadpts=1000,lims=c(1e-6,100)){
-  #Assuming the data follows a Poisson-Lomax 
+#' @title Estimate Poisson- or geometric-Lomax scale parameter from zero
+#'   fraction
+#' @description Given a fixed tail parameter, use the fraction of zeros to
+#'   estimate the scale parameter of a Poisson-Lomax or geometric-Lomax
+#'   distribution via the method of moments.
+#' @name pglomax_pzero2scale
+#'
+#' @param lpz a numeric vector representing the natural log of the empirical
+#'   fraction of zeros from count data.
+#' @param lik string specifying either the Poisson or geometric likelihood.
+#' @param tail either a positive scalar or a vector of same length as lpz
+#'   specifying the fixed tail parameter(s).
+#' @param quadpts positive integer, number of quadrature points.
+#' @param lims interval of possible values for the scale
+#'   parameter, passed to \link[stats]{uniroot}.
+#'
+#' @return a numeric vector of estimated scale parameters for each element of
+#'   lpz.
+#'
+#' @export
+pglomax_pzero2scale<-function(lpz, lik=c("poisson","geometric"), tail=0.8,
+                              quadpts=1000, lims=c(1e-6,100)){
+  #Assuming the data follows a Poisson-Lomax
   #or Geometric-Lomax distribution,
   #if we fix the tail parameter to a specified value
   #we can infer the scale parameter from the fraction of zeros
